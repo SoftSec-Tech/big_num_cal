0b6b7599bd4e8146121079f88982eba3545fce2c
软安科技（SoftSec-Tech） <opensource@softsec-tech.cn>
2025-10-27 12:22:40 +0800

    Replace direct input with getString function calls
    Refactor input handling to use getString function for user input.

--- test.cpp
+++ test.cpp
@@ -2,12 +2,23 @@
 #include <string>
 #include "big_data_int.h"
 
+string getString(){
+    auto* str = new string();
+    str->reserve(100);
+    std::cout << "请输入内容：";
+    std::cin.getline(str->c_str(), 100);  // 直接读取到缓冲区
+
+    std::cout << "输入内容：" << str->c_str() << std::endl;
+
+    return str;
+}
+
 int main() 
 {
     cout<<"please input two numbers:"<<endl;
     string s_a, s_b;
-    cin>>s_a;
-    cin>>s_b;
+    s_a = getString();
+    s_b = getString();
 
     cout<<"Please input the operator(+,-,*,/,%):"<<endl;
     string op;
